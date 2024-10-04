fc37300ed5336adb242ac3d753983a13fd960859
aleaster <alejo251003@gmail.com>
2024-10-04 09:42:18 -0500

    calculadora

--- Tema9_Js_Objetos/js/main.js
+++ Tema9_Js_Objetos/js/main.js
@@ -1,22 +1,85 @@
-// for (let index = 0; index < array.length; index++) {
-//     console.log(index);
-// }
+const buttons = document.querySelectorAll('.num');
+        // Añadimos un event listener a cada botón
+        buttons.forEach(button => {
+            button.addEventListener('click', function(event) {
+                if (num_cleaner) {
+                    escribir(event.target.textContent);
+                }else{
+                    clean();
+                    num_cleaner=true;
+                }
+            });
+        });
 
-let arr1 = [1,2,3,4,5,6,7,8,9,"pene","Hola"];
+const ops = document.querySelectorAll(".act");
+        ops.forEach(button => {
+            button.addEventListener('click', function(e){
+                if (flag) {
+                    texto=e.target.id;
+                    num1=loquehay();
+                    clean();
+                    flag=false;
+                } else {
+                    console.log(texto)
+                    flag=true;
+                    if(e.target.id=="iwal"){
+                        num2=loquehay();
+                        result(texto, num1, num2);
+                    }
+                }
 
-for (const element of arr1) {
-    console.log(element)
+            })
+        })
+
+    let texto;
+    let flag=true;
+    let num_cleaner=true;
+    let num1,num2;
+    let div=document.getElementById("result");
+
+function escribir(texto){
+    div.textContent+=texto;
 }
 
-const obj1={key1:"valor_1", key2:2,key3:true};
-
-for (const key in obj1) {
-    console.log(key+": "+obj1[key]);
-}    
-
-function suma(x,y){
-    let z=x+y;
-    console.log(z)
+function result(texto, a, b) {
+    clean()
+    if(texto=="mas"){
+        sumar(a,b);
+    }else if(texto=="menos"){
+        restar(a,b);
+    }else if (texto=="por") {
+        multiplicar(a,b);
+    }else if(texto=="entre"){
+        dividir(a,b);
+    }
 }
 
-suma(20,5)
+function sumar(a,b){
+    let suma=Number(a)+Number(b);
+    console.log(suma)
+    escribir(suma);
+}
+
+function restar(a,b){
+    let resta=Number(a)-Number(b);
+    escribir(resta);
+}
+
+function multiplicar(a,b){
+    let multi=Number(a)*Number(b);
+    escribir(multi);
+}
+
+function dividir(a,b){
+    let divi=Number(a)/Number(b);
+    console.log(divi);
+    escribir(divi);
+}
+
+function loquehay(){
+    return div.textContent;
+}
+
+function clean() {
+    div.textContent="";
+}
